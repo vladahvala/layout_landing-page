b823f4e4ba344b26608ffed91623edb227ea4dba
vladahvala <80634246+vladahvala@users.noreply.github.com>
2025-12-07 00:15:30 +0200

    finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  const phoneIcon = document.querySelector('.icon--phone');
+  const numbers = document.querySelector('.top-bar__info');
+
+  phoneIcon.addEventListener('click', () => {
+    numbers.classList.toggle('is-visible');
+  });
+});
