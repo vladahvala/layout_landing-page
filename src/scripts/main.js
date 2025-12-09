6048401750bbb57684473fd896216d2534fff845
vladahvala <80634246+vladahvala@users.noreply.github.com>
2025-12-09 21:34:49 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const phoneIcon = document.querySelector('.icon--phone');
-  const numbers = document.querySelector('.top-bar__info');
-
-  phoneIcon.addEventListener('click', () => {
-    numbers.classList.toggle('is-visible');
-  });
-});
-
 const form = document.querySelector('.contact__form');
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
   this.reset();
+});
+
+const sendButton = document.querySelector('.contact__form-button');
+
+sendButton.addEventListener('click', () => {
+  form.reset();
 });
