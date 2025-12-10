31ca58731b23cfbdd63f8848baf81a3cbf01593c
vladahvala <80634246+vladahvala@users.noreply.github.com>
2025-12-10 15:04:32 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,3 +10,15 @@ const sendButton = document.querySelector('.contact__form-button');
 sendButton.addEventListener('click', () => {
   form.reset();
 });
+
+// Phone nums visibility
+const info = document.querySelector('.top-bar__info');
+const iconsWrapper = document.querySelector('.top-bar__icons');
+
+iconsWrapper.addEventListener('mouseenter', () => {
+  info.classList.add('is-visible');
+});
+
+iconsWrapper.addEventListener('mouseleave', () => {
+  info.classList.remove('is-visible');
+});
